c37b6ce6dc8dc28d3393767cf6560088a75d557c
prnvthir14 <prnvthir@gmail.com>
2021-02-04 17:48:35 +0000

    updated to parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -25,18 +25,25 @@ pool.connect( (err)=> {
 
 // console.log(pool)
 
-let cohort_id_to_find = process.argv[2]
-// console.log(cohort_id_to_find)
-let limit = process.argv[3]
-// console.log(limit)
-//PROMISE METHOD TO QUERY
-pool.query(`
+// let cohort_id_to_find = process.argv[2]
+// // console.log(cohort_id_to_find)
+// let limit = process.argv[3]
+// // console.log(limit)
+// //PROMISE METHOD TO QUERY
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
